cf0d27dd88e57fd81088e7f9727d040cefc4ea1f
James King <james_andrew_king@hotmail.co.uk>
2021-05-28 13:54:21 -0700

    finished first draft or a carousel component

---
+++ src/assets/images/placeholder-images.js
@@ -0,0 +1,19 @@
+const sliderData = [
+    {
+        image : "https://images.unsplash.com/photo-1533109721025-d1ae7ee7c1e1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29sb3VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image : "https://images.unsplash.com/photo-1512928210967-3dced5ba507b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29sb3VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image : "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+    },
+    {
+        image : "https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=653&q=80"
+    },
+    {
+        image : "https://images.unsplash.com/photo-1508898578281-774ac4893c0c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNvbG91cnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    }
+];
+
+export default sliderData;
