ccb2a97560bf6cb738cae871290f38b78c3e8d67
James King <james_andrew_king@hotmail.co.uk>
2021-06-09 14:32:04 -0700

    added bio section to bio page

--- src/components/hero-about/HeroAbout.js
+++ src/components/hero-about/HeroAbout.js
@@ -4,11 +4,13 @@ import profilePic from '../../assets/images/temp-profile-img.jpg';
 function HeroAbout() {
     return (
         <section className="about-hero">
-            <div className="about-hero__column">
-                <img className="about-hero__profile-pic" src={profilePic}  alt="This is me" />
-            </div>
-            <div className="about-hero__column">
-                <h1>I'm James</h1>
+            <div className="about-hero__content">
+                <div className="about-hero__column">
+                    <img className="about-hero__profile-pic" src={profilePic}  alt="This is me" />
+                </div>
+                <div className="about-hero__column">
+                    <h1>I'm James</h1>
+                </div>
             </div>
         </section>
     );
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,6 +5,11 @@ function AboutPage() {
     return (
         <main className="about-pg">
             <HeroAbout />
+            <section className="bio">
+                <p className="bio__text">Hi! Thank you for visiting my site. I'm a Full Stack Web Developer located in Kelowna, British Columbia.</p>
+                <p className="bio__text">With close to 8 years experince in Sales and Customer Success, I always enjoyed thinking outside the box to find new and interesting ways to solve problems</p>
+                <p className="bio__text">After being introduced to Python in 2018 I was instantly hooked. I am now focusing on Web Development to pursue my passion building things</p>
+            </section>
         </main>
     );
 }
