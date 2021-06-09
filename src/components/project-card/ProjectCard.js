a1545171b906fde1f18b360db912f999a9c6540c
James King <james_andrew_king@hotmail.co.uk>
2021-06-09 12:54:37 -0700

    rebuilt and finalised style of  project card component and added transitions for better experince

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -1,14 +1,24 @@
 import './ProjectCard.scss';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import GithubIcon from '../../assets/icons/github.svg'
 
 function ProjectCard() {
 
     return (
         <li className="project-card">
-            <Link className="project-card__link" to="/">
-                cuby-Fi
-            </Link>
+            <h3 className="project-card__title">Project</h3>
+            <div className="project-card__links">
+                <a className="project-card__link" href="#">
+                    <img className="project-card__icon" src={GithubIcon} alt="see my code at GitHub" />
+                    <p>Check out my Code at GitHub</p>
+                </a>
+                <a className="project-card__link" href="#">
+                    <img className="project-card__icon" src={GithubIcon} alt="see my code at GitHub" />
+                    <p>Check out my Code at GitHub</p>
+                </a>
+            </div>
         </li>
+
     );
 
 }
