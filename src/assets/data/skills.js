c83826f19d141d76838add9d3b110c076e53c001
James King <james_andrew_king@hotmail.co.uk>
2021-06-10 11:24:01 -0700

    created a skills js file, icons now served staticly from here'

---
+++ src/assets/data/skills.js
@@ -0,0 +1,52 @@
+import ReactIcon from '../icons/react.svg';
+import HTMLIcon from '../icons/html5.svg';
+import CSSIcon from '../icons/css3.svg';
+import ExpressIcon from '../icons/express.svg';
+import JavaScriptIcon from '../icons/javascript.svg';
+import NodeIcon from '../icons/nodedotjs.svg';
+import PythonIcon from '../icons/python.svg';
+import ReduxIcon from '../icons/redux.svg';
+import TensorFlowIcon from '../icons/tensorflow.svg';
+
+
+export const skillsArrFrontend = [
+    {
+        skill : 'html',
+        icon : HTMLIcon
+    },
+    {
+        skill : 'css',
+        icon : CSSIcon
+    },
+    {
+        skill : 'javascript',
+        icon : JavaScriptIcon
+    },
+    {
+        skill : 'react',
+        icon : ReactIcon
+    },
+    {
+        skill : 'redux',
+        icon : ReduxIcon
+    },
+    {
+        skill : 'tensorflow.js',
+        icon : TensorFlowIcon
+    }
+]
+
+export const skillsArrBackend = [
+    {
+        skill : 'node.js',
+        icon : NodeIcon
+    },
+    {
+        skill : 'express.js',
+        icon : ExpressIcon
+    },
+    {
+        skill : 'python',
+        icon : PythonIcon
+    }
+]
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -3,6 +3,16 @@ import { useState } from 'react';
 import SectionHeader from '../section-header/SectionHeader';
 import SkillsToggle from '../skills-toggle/SkillsToggle';
 import SkillsList from '../skills-list/SkillsList';
+import ReactIcon from '../../assets/icons/react.svg';
+import HTMLIcon from '../../assets/icons/html5.svg';
+import CSSIcon from '../../assets/icons/css3.svg';
+import ExpressIcon from '../../assets/icons/express.svg';
+import JavaScriptIcon from '../../assets/icons/javascript.svg';
+import NodeIcon from '../../assets/icons/nodedotjs.svg';
+import PythonIcon from '../../assets/icons/python.svg';
+import reduxIcon from '../../assets/icons/redux.svg';
+import TensorFlowIcon from '../../assets/icons/tensorflow.svg';
+import { skillsArrFrontend, skillsArrBackend } from '../../assets/data/skills';
 
 function Skills() {
 
@@ -10,15 +20,17 @@ function Skills() {
 
     const onRadioSelect = e => setSelectedSkill(e.target.value);
 
-    const skillsArrFrontend = ["HTML", "CSS", "JavaScript", "Tensorflow.js", "React.js", "Redux"];
-    const skillsArrBackend = ["Node.js", "Express.js", "Python"];
+    // const skillsArrFrontend = ["HTML", "CSS", "JavaScript", "Tensorflow.js", "React.js", "Redux"];
+    // const skillsArrBackend = ["Node.js", "Express.js", "Python"];
 
     return (
         <section className="skills">
             <div className="skills__content">
                 <SectionHeader heading="MY SKILLS" />
                 <SkillsToggle onRadioSelect={onRadioSelect} selectedSkill={selectedSkill}/>
-                {selectedSkill === 'frontend' ? <SkillsList listName="Front End Skills" skillsArr={skillsArrFrontend}/> : <SkillsList listName="Back End Skills" skillsArr={skillsArrBackend}/>}
+                {selectedSkill === 'frontend' ? 
+                <SkillsList listName="Front End Skills" skillsArr={skillsArrFrontend}/> : 
+                <SkillsList listName="Back End Skills" skillsArr={skillsArrBackend}/>}
             </div>
         </section>
     );
