42b71c9b6e474bdc07ef24e329817d25356f0291
James King <james_andrew_king@hotmail.co.uk>
2021-06-15 22:02:41 -0700

    relocated img tag to outside p tag

--- src/components/copyright-bar/CopyrightBar.js
+++ src/components/copyright-bar/CopyrightBar.js
@@ -4,9 +4,11 @@ import CopyrightIcon from '../../assets/icons/Copyright.svg';
 function CopyrightBar() {
     return (
         <div className="copyright-bar">
-            <p className="copyright-bar__text"><img className="copyright-bar__icon" src={CopyrightIcon} alt="copyright" />2021 JAMES KING</p>
+            <div className="copyright-bar__text-icon-container">
+                <img className="copyright-bar__icon" src={CopyrightIcon} alt="copyright" />
+                <p className="copyright-bar__text">2021 JAMES KING</p>
+            </div>
             <p className="copyright-bar__text">All rights reserved</p>
-            
         </div>
     );
 }
