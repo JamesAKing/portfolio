284d942eee1c9bc2e1ff3215df9b2580929ba545
James King <james_andrew_king@hotmail.co.uk>
2021-06-15 18:11:40 -0700

    removed thank you message pending redesign

--- src/components/thank-you/ThankYou.js
+++ src/components/thank-you/ThankYou.js
@@ -9,10 +9,10 @@ function ThankYou() {
                         <span className="thank-you__text--highlight">DROP ME A LINE,</span> I'D LIKE TO HEAR FROM YOU
                     </h2>
                 </div>
-                <div className="thank-you__text thank-you__msg-container">
-                    {/* <img src="#" alt="random" /> */}
+                {/* <div className="thank-you__text thank-you__msg-container">
+                    <img src="#" alt="random" />
                     <p className="thank-you__msg">...and thank you for scrolling to the bottom!</p>
-                </div>
+                </div> */}
             </div>
         </section>
     );
