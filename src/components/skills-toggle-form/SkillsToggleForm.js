12c8a6f39cf14674a992523017b4c5ef6e88d31d
James King <james_andrew_king@hotmail.co.uk>
2021-06-15 21:18:23 -0700

    minor visual bug fixes

--- src/components/skills-toggle-form/SkillsToggleForm.js
+++ src/components/skills-toggle-form/SkillsToggleForm.js
@@ -4,11 +4,11 @@ function SkillsToggleForm({ onRadioSelect, selectedSkill }) {
 
     return (
         <form className="skills-form" name="skillsToggle">
-            <label className={`skills-form__label ${selectedSkill === "frontend" ? "skills-form__label--selected" : null}`} htmlFor="frontend">
+            <label className={`skills-form__label ${selectedSkill === "frontend" ? "skills-form__label--selected" : ""}`} htmlFor="frontend">
                 Front End
                 <input className="skills-form__radio" type="radio" id="frontend" name="skillsToggle" value="frontend" onChange={onRadioSelect}/>
             </label>
-            <label className={`skills-form__label ${selectedSkill === "backend" ? "skills-form__label--selected" : null}`} htmlFor="backend">
+            <label className={`skills-form__label ${selectedSkill === "backend" ? "skills-form__label--selected" : ""}`} htmlFor="backend">
                 Back End
                 <input className="skills-form__radio" type="radio" id="backend" name="skillsToggle" value="backend" onChange={onRadioSelect}/>
             </label>
