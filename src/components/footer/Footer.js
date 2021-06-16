4056e5c4f5ec2e8b74c730b02b0dd84233d3fcaa
James King <james_andrew_king@hotmail.co.uk>
2021-06-15 17:41:06 -0700

    added a thank section to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,22 @@
 import './Footer.scss';
+import ThankYou from '../thank-you/ThankYou';
 import Logo from '../logo/Logo';
 import SocialsNav from '../socials-nav/SocialsNav';
 
 function Footer() {
 
     return (
-        <footer className="footer">
-            <div className="footer__content">
-                <div className="footer__logo">
-                    <Logo alt={true}/>
+        <>
+            <ThankYou />
+            <footer className="footer">
+                <div className="footer__content">
+                    <div className="footer__logo">
+                        <Logo alt={true}/>
+                    </div>
+                    <SocialsNav />
                 </div>
-                <SocialsNav />
-            </div>
-        </footer>
+            </footer>
+        </>
     );
 
 }
---
+++ src/components/thank-you/ThankYou.js
@@ -0,0 +1,21 @@
+import './ThankYou.scss';
+
+function ThankYou() {
+    return (
+        <section className="thank-you">
+            <div className="thank-you__content">
+                <div>
+                    <h2 className="thank-you__text">
+                        <span className="thank-you__text--highlight">DROP ME A LINE,</span> I'D LIKE TO HEAR FROM YOU
+                    </h2>
+                </div>
+                <div className="thank-you__text thank-you__msg-container">
+                    {/* <img src="#" alt="random" /> */}
+                    <p className="thank-you__msg">...and thank you for scrolling to the bottom!</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default ThankYou;
