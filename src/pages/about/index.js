d73c44c02d20c0bf1b92a71c5d15b206f788239d
James King <james_andrew_king@hotmail.co.uk>
2021-08-16 12:41:10 -0700

    updated copy

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -11,6 +11,8 @@ function AboutPage() {
                 <p className="bio__text">as a tech lover with an analytic mind, I have always been interested in knowing how things work...</p>
                 <p className="bio__text">and with close to 8 years experience in Real Estate, Sales, and Customer Success, I always enjoyed thinking outside the box to find creative and interesting ways to solve problems...</p>
                 <p className="bio__text">I decided to start teaching myself Python in 2018 and was instantly hooked...</p>
+                <p className="bio__text">I recently graduated with a diploma in Full Stack Web Development through which I further developed my skills and understanding of key principles and concepts whilst simultaneously applying this knowledge to projects both personal and collaboratively with professionals across multiple disciplines and companies including Google...</p>
+                <p className="bio__text">Currently, I am working as a Full Stack Developer on several projects including Climate Mind, an open source, Stanford University backed project that facilitates constructive conversations on Climate Change through education and connection based on personal values...</p>
                 <p className="bio__text">I am now focusing on combining my years of sales and customer success experience with my developer skill set to build exciting and innovative creations that help solve our everyday problems.</p>
             </section>
             <Skills />
