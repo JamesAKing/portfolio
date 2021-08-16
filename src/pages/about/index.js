bde9d62b6ca38acc72bf2893ea64c3c868c6c4ad
James King <james_andrew_king@hotmail.co.uk>
2021-08-16 12:43:54 -0700

    updated copy to include moving back to the UK

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -7,7 +7,7 @@ function AboutPage() {
         <main className="about-pg">
             <HeroAbout />
             <section className="bio">
-                <p className="bio__text">I'm a Full Stack Web Developer located in Kelowna, British Columbia...</p>
+                <p className="bio__text">I'm a Full Stack Web Developer located in Kelowna, British Columbia and relocating back to the UK in September...</p>
                 <p className="bio__text">as a tech lover with an analytic mind, I have always been interested in knowing how things work...</p>
                 <p className="bio__text">and with close to 8 years experience in Real Estate, Sales, and Customer Success, I always enjoyed thinking outside the box to find creative and interesting ways to solve problems...</p>
                 <p className="bio__text">I decided to start teaching myself Python in 2018 and was instantly hooked...</p>
