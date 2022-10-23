dcd43e74f0602c9c77bbf54da77a2f30067bc953
James King <jamesking@catalystvr.com.au>
2022-10-23 13:57:26 +0100

    made copyright date dynamic

--- src/components/copyright-bar/CopyrightBar.js
+++ src/components/copyright-bar/CopyrightBar.js
@@ -1,16 +1,22 @@
-import './CopyrightBar.scss';
-import CopyrightIcon from '../../assets/icons/Copyright.svg';
+import "./CopyrightBar.scss";
+import CopyrightIcon from "../../assets/icons/Copyright.svg";
 
 function CopyrightBar() {
-    return (
-        <div className="copyright-bar">
-            <div className="copyright-bar__text-icon-container">
-                <img className="copyright-bar__icon" src={CopyrightIcon} alt="copyright" />
-                <p className="copyright-bar__text">2021 JAMES KING</p>
-            </div>
-            <p className="copyright-bar__text">All rights reserved</p>
-        </div>
-    );
+  return (
+    <div className="copyright-bar">
+      <div className="copyright-bar__text-icon-container">
+        <img
+          className="copyright-bar__icon"
+          src={CopyrightIcon}
+          alt="copyright"
+        />
+        <p className="copyright-bar__text">
+          {new Date().getFullYear() || 2022} JAMES KING
+        </p>
+      </div>
+      <p className="copyright-bar__text">All rights reserved</p>
+    </div>
+  );
 }
 
 export default CopyrightBar;
