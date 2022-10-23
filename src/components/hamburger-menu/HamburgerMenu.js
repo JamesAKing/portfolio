81db8623c26d882ed39187535005678f5b1d3c9a
James King <jamesking@catalystvr.com.au>
2022-10-23 18:18:11 +0100

    added react-icons as dependency. replaced close menu icon with HQ asset

--- src/components/hamburger-menu/HamburgerMenu.js
+++ src/components/hamburger-menu/HamburgerMenu.js
@@ -1,19 +1,17 @@
-import './HamburgerMenu.scss';
-import { useState } from 'react';
-import HamburgerIcon from './HamburgerIcon';
-import HiddenMenu from './HiddenMenu';
+import "./HamburgerMenu.scss";
+import { useState } from "react";
+import HamburgerIcon from "./HamburgerIcon";
+import HiddenMenu from "./HiddenMenu";
 
 function HamburgerMenu() {
+  const [menuActive, setMenuActive] = useState(true);
 
-    const [ menuActive, setMenuActive ] = useState(false);
-
-    return (
-        <div className="hamburger-menu">
-            <HamburgerIcon setMenuActive={setMenuActive}/>
-            {menuActive && <HiddenMenu setMenuActive={setMenuActive}/>}
-        </div>
-    );
-
+  return (
+    <div className="hamburger-menu">
+      <HamburgerIcon setMenuActive={setMenuActive} />
+      {menuActive && <HiddenMenu setMenuActive={setMenuActive} />}
+    </div>
+  );
 }
 
 export default HamburgerMenu;
