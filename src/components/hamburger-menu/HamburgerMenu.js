7cf99ad8f3283a2e646f4169d244a0e740294cd4
James King <jamesking@catalystvr.com.au>
2022-10-23 19:29:23 +0100

    burger menu has better, matching assets

--- src/components/hamburger-menu/HamburgerMenu.js
+++ src/components/hamburger-menu/HamburgerMenu.js
@@ -1,14 +1,33 @@
 import "./HamburgerMenu.scss";
-import { useState } from "react";
-import HamburgerIcon from "./HamburgerIcon";
+import { useEffect, useState } from "react";
 import HiddenMenu from "./HiddenMenu";
+
+import { IoIosMenu } from "react-icons/io";
 
 function HamburgerMenu() {
   const [menuActive, setMenuActive] = useState(true);
 
+  useEffect(() => {
+    /** Prevent body from scrolling when modal is open */
+    const body = document.body;
+
+    if (menuActive) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "unset";
+    }
+  }, [menuActive]);
+
   return (
     <div className="hamburger-menu">
-      <HamburgerIcon menuActive={menuActive} setMenuActive={setMenuActive} />
+      <button
+        className={`hamburger-menu__open-btn ${
+          menuActive ? "rotate90" : "rotate0"
+        }`}
+        onClick={() => setMenuActive(true)}
+      >
+        <IoIosMenu />
+      </button>
       <HiddenMenu menuActive={menuActive} setMenuActive={setMenuActive} />
     </div>
   );
