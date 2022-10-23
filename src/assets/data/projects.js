021cd7fdedabc4844680b7c9f610d8c906a7bbd1
James King <jamesking@catalystvr.com.au>
2022-10-23 12:31:10 +0100

    ran firebase init and deployed to live site

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,63 +1,72 @@
-import CubyFiIcon from '../project-icons/cuby-fi.svg';
-import UberEastZooLogo from '../project-icons/ubereats.svg';
+import CubyFiIcon from "../project-icons/cuby-fi.svg";
+import UberEastZooLogo from "../project-icons/ubereats.svg";
 
-import { myGithubURL } from '../../utilities/URLS'; 
+import { myGithubURL } from "../../utilities/URLS";
 
 export const projectsArr = [
-    // {
-    //     projectTitle : 'Uber Eats Zoo',
-    //     projectDesc : null,
-    //     projectLogo : UberEastZooLogo,
-    //     githubURL : null,
-    //     websiteURL: 'https://ubereatszoo.com.au/',
-    //     projectVideo : null
-    // },
-    {
-        projectTitle : 'Aussie Animal Experince',
-        projectDesc : null,
-        projectLogo : null,
-        githubURL : null,
-        websiteURL: 'https://aussieanimalquest.com.au/',
-        projectVideo : null
-    },
-    {
-        projectTitle : 'cuby-Fi',
-        projectDesc : null,
-        projectLogo : CubyFiIcon,
-        githubURL : `${myGithubURL}/cuby-fi`,
-        websiteURL: null,
-        projectVideo : '../video/cubyFi.mp4'
-    },
-    {
-        projectTitle : 'Climate Mind',
-        projectDesc : null,
-        projectLogo : null,
-        githubURL : null,
-        websiteURL: "https://climatemind.org/",
-        projectVideo : null
-    },
-    {
-        projectTitle : 'Portfolio',
-        projectDesc : null,
-        projectLogo : null,
-        githubURL : `${myGithubURL}/portfolio`,
-        websiteURL: null,
-        projectVideo : null
-    },
-    {
-        projectTitle : 'Rhodes Central Voucher Page',
-        projectDesc : null,
-        projectLogo : null,
-        githubURL : null,
-        websiteURL: 'http://rhodes-landing-page.s3-website-ap-southeast-2.amazonaws.com/',
-        projectVideo : null
-    },
-    {
-        projectTitle : 'Pirates of Atlassian Backend Leaderboard',
-        projectDesc : null,
-        projectLogo : null,
-        githubURL : null,
-        websiteURL: null,
-        projectVideo : null
-    }
-]
+  {
+    projectTitle: "Uber Eats Zoo",
+    projectDesc: null,
+    projectLogo: UberEastZooLogo,
+    githubURL: null,
+    websiteURL: "https://ubereatszoo.com.au/",
+    projectVideo: null,
+  },
+  {
+    projectTitle: "Aussie Animal Experince",
+    projectDesc: null,
+    projectLogo: null,
+    githubURL: null,
+    websiteURL: "https://aussieanimalquest.com.au/",
+    projectVideo: null,
+  },
+  {
+    projectTitle: "cuby-Fi",
+    projectDesc: null,
+    projectLogo: CubyFiIcon,
+    githubURL: `${myGithubURL}/cuby-fi`,
+    websiteURL: null,
+    projectVideo: "../video/cubyFi.mp4",
+  },
+  {
+    projectTitle: "Climate Mind",
+    projectDesc: null,
+    projectLogo: null,
+    githubURL: null,
+    websiteURL: "https://climatemind.org/",
+    projectVideo: null,
+  },
+  {
+    projectTitle: "Portfolio",
+    projectDesc: null,
+    projectLogo: null,
+    githubURL: `${myGithubURL}/portfolio`,
+    websiteURL: null,
+    projectVideo: null,
+  },
+  {
+    projectTitle: "Rhodes Central Vouchers",
+    projectDesc: null,
+    projectLogo: null,
+    githubURL: null,
+    // websiteURL: 'http://rhodes-landing-page.s3-website-ap-southeast-2.amazonaws.com/',
+    websiteURL: null,
+    projectVideo: null,
+  },
+  {
+    projectTitle: "Pirates of Atlassian Backend Leaderboard",
+    projectDesc: null,
+    projectLogo: null,
+    githubURL: null,
+    websiteURL: null,
+    projectVideo: null,
+  },
+  {
+    projectTitle: "Cricket World Cup VR",
+    projectDesc: null,
+    projectLogo: null,
+    githubURL: null,
+    websiteURL: null,
+    projectVideo: null,
+  },
+];
