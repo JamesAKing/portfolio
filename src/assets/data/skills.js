23d3c692431217be23d1378c9b1242f01635fb28
James King <jamesking@catalystvr.com.au>
2022-10-23 11:52:35 +0100

    updated skills lists

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -1,168 +1,162 @@
-import ReactIcon from '../icons/react.svg';
-import TypeScriptIcon from '../icons/typescript.svg';
-import HTMLIcon from '../icons/html5.svg';
-import CSSIcon from '../icons/css3.svg';
-import ExpressIcon from '../icons/express.svg';
-import JavaScriptIcon from '../icons/javascript.svg';
-import NodeIcon from '../icons/nodedotjs.svg';
-import PythonIcon from '../icons/python.svg';
-import ReduxIcon from '../icons/redux.svg';
-import NextIcon from '../icons/nextdotjs.svg';
-import TensorFlowIcon from '../icons/tensorflow.svg';
-import MySQLIcon from '../icons/mysql.svg';
-import GitIcon from '../icons/git.svg';
-import GithubIcon from '../icons/github-alt.svg';
-import HerokuIcon from '../icons/heroku.svg';
-import NPMIcon from '../icons/npm.svg';
-import WebpackIcon from '../icons/webpack.svg';
-import SassIcon from '../icons/sass.svg';
-import BEMIcon from '../icons/BEM.svg';
-import OAuthIcon from '../icons/oauth.svg';
-import AuthIcon from '../icons/authentication.svg';
-import APIsIcon from '../icons/apis.svg';
-import DockerIcon from '../icons/docker.svg';
-import DOMIcon from '../icons/dom.svg';
-import RespDesignIcon from '../icons/responsive-design.svg';
-import AwsIcon from '../icons/amazonaws.svg';
-import FirebaseIcon from '../icons/firebase.svg';
-import ThreeJSIcon from '../icons/threedotjs.svg'
-
-
+import ReactIcon from "../icons/react.svg";
+import TypeScriptIcon from "../icons/typescript.svg";
+import HTMLIcon from "../icons/html5.svg";
+import CSSIcon from "../icons/css3.svg";
+import ExpressIcon from "../icons/express.svg";
+import JavaScriptIcon from "../icons/javascript.svg";
+import NodeIcon from "../icons/nodedotjs.svg";
+// import PythonIcon from "../icons/python.svg";
+// import ReduxIcon from "../icons/redux.svg";
+import NextIcon from "../icons/nextdotjs.svg";
+// import TensorFlowIcon from "../icons/tensorflow.svg";
+// import MySQLIcon from "../icons/mysql.svg";
+import GitIcon from "../icons/git.svg";
+import GithubIcon from "../icons/github-alt.svg";
+import HerokuIcon from "../icons/heroku.svg";
+import NPMIcon from "../icons/npm.svg";
+import WebpackIcon from "../icons/webpack.svg";
+import SassIcon from "../icons/sass.svg";
+import BEMIcon from "../icons/BEM.svg";
+import OAuthIcon from "../icons/oauth.svg";
+import AuthIcon from "../icons/authentication.svg";
+import APIsIcon from "../icons/apis.svg";
+import DockerIcon from "../icons/docker.svg";
+import DOMIcon from "../icons/dom.svg";
+import RespDesignIcon from "../icons/responsive-design.svg";
+import AwsIcon from "../icons/amazonaws.svg";
+import FirebaseIcon from "../icons/firebase.svg";
+import ThreeJSIcon from "../icons/threedotjs.svg";
 
 export const skillsObjFrontend = {
-    skillsArrName : 'Front End Skills',
-    skillsArr : [
-        {
-            skill : 'html',
-            icon : HTMLIcon
-        },
-        {
-            skill : 'css',
-            icon : CSSIcon
-        },
-        {
-            skill : 'javascript',
-            icon : JavaScriptIcon
-        },
-        {
-            skill : 'three js',
-            icon : ThreeJSIcon
-        },
-        {
-            skill : 'typescript',
-            icon : TypeScriptIcon
-        },
-        {
-            skill : 'react',
-            icon : ReactIcon
-        },
-        // {
-        //     skill : 'redux',
-        //     icon : ReduxIcon
-        // },
-        {
-            skill : 'next JS',
-            icon : NextIcon
-        },
-        {
-            skill : 'tensorflow.js',
-            icon : TensorFlowIcon
-        }
-    ]
-}
+  skillsArrName: "Front End Skills",
+  skillsArr: [
+    {
+      skill: "html",
+      icon: HTMLIcon,
+    },
+    {
+      skill: "css",
+      icon: CSSIcon,
+    },
+    {
+      skill: "javascript",
+      icon: JavaScriptIcon,
+    },
+    {
+      skill: "three js",
+      icon: ThreeJSIcon,
+    },
+    {
+      skill: "typescript",
+      icon: TypeScriptIcon,
+    },
+    {
+      skill: "react",
+      icon: ReactIcon,
+    },
+    {
+      skill: "next JS",
+      icon: NextIcon,
+    },
+    {
+      skill: "zustand",
+      icon: ReactIcon,
+    },
+  ],
+};
 
 export const skillsObjBackend = {
-    skillsArrName : 'Back End Skills',
-    skillsArr : [
-        {
-            skill : 'node.js',
-            icon : NodeIcon
-        },
-        {
-            skill : 'express.js',
-            icon : ExpressIcon
-        },
-        // {
-        //     skill : 'python',
-        //     icon : PythonIcon
-        // },
-        // {
-        //     skill : 'mysql',
-        //     icon : MySQLIcon
-        // },
-        {
-            skill : 'next JS',
-            icon : NextIcon
-        },
-    ]
-}
+  skillsArrName: "Back End Skills",
+  skillsArr: [
+    {
+      skill: "node.js",
+      icon: NodeIcon,
+    },
+    {
+      skill: "express.js",
+      icon: ExpressIcon,
+    },
+    // {
+    //     skill : 'python',
+    //     icon : PythonIcon
+    // },
+    // {
+    //     skill : 'mysql',
+    //     icon : MySQLIcon
+    // },
+    {
+      skill: "next JS",
+      icon: NextIcon,
+    },
+  ],
+};
 
 export const skillsObjIndustry = {
-    skillsArrName : 'Industry Best Practices',
-    skillsArr : [
-        {
-            skill : 'Amazon AWS',
-            icon : AwsIcon
-        },
-        {
-            skill : 'Firebase',
-            icon : FirebaseIcon
-        },
-        {
-            skill : 'git',
-            icon : GitIcon
-        },
-        {
-            skill : 'github',
-            icon : GithubIcon
-        },
-        {
-            skill : ' REST api\'s',
-            icon : APIsIcon
-        },
-        // {
-        //     skill : 'web api\'s',
-        //     icon : APIsIcon
-        // },
-        {
-            skill : 'heroku',
-            icon : HerokuIcon
-        },
-        {
-            skill : 'document object model',
-            icon : DOMIcon
-        },
-        {
-            skill : 'npm',
-            icon : NPMIcon
-        },
-        {
-            skill : 'webpack',
-            icon : WebpackIcon
-        },
-        {
-            skill : 'user authentication',
-            icon : AuthIcon
-        },
-        {
-            skill : 'OAuth',
-            icon : OAuthIcon
-        },
-        {
-            skill : 'scss',
-            icon : SassIcon
-        },
-        {
-            skill : 'BEM',
-            icon : BEMIcon
-        },
-        {
-            skill : 'responsive design',
-            icon : RespDesignIcon
-        },
-        {
-            skill : 'docker',
-            icon : DockerIcon
-        }
-    ]
-}
+  skillsArrName: "Industry Best Practices",
+  skillsArr: [
+    {
+      skill: "Amazon AWS",
+      icon: AwsIcon,
+    },
+    {
+      skill: "Firebase",
+      icon: FirebaseIcon,
+    },
+    // {
+    //   skill: "git",
+    //   icon: GitIcon,
+    // },
+    {
+      skill: "github",
+      icon: GithubIcon,
+    },
+    // {
+    //   skill: " REST api's",
+    //   icon: APIsIcon,
+    // },
+    // {
+    //     skill : 'web api\'s',
+    //     icon : APIsIcon
+    // },
+    // {
+    //   skill: "heroku",
+    //   icon: HerokuIcon,
+    // },
+    // {
+    //   skill: "document object model",
+    //   icon: DOMIcon,
+    // },
+    // {
+    //   skill: "npm",
+    //   icon: NPMIcon,
+    // },
+    // {
+    //   skill: "webpack",
+    //   icon: WebpackIcon,
+    // },
+    {
+      skill: "user authentication",
+      icon: AuthIcon,
+    },
+    {
+      skill: "OAuth",
+      icon: OAuthIcon,
+    },
+    {
+      skill: "scss",
+      icon: SassIcon,
+    },
+    {
+      skill: "BEM",
+      icon: BEMIcon,
+    },
+    {
+      skill: "responsive design",
+      icon: RespDesignIcon,
+    },
+    // {
+    //   skill: "docker",
+    //   icon: DockerIcon,
+    // },
+  ],
+};
--- src/components/hexagon-graphic/HexagonGraphic.js
+++ src/components/hexagon-graphic/HexagonGraphic.js
@@ -1,42 +1,45 @@
-import './HexagonGraphic.scss';
-import React, { useState, useRef, useEffect } from 'react';
+import "./HexagonGraphic.scss";
+import React, { useState, useRef, useEffect } from "react";
 
 function HexagonGraphic() {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisable] = useState(false);
+  // const [ animated, setAnimated ] = useState(false)
 
-    const containerRef = useRef(null);
-    const [ isVisible, setIsVisable ] = useState(false);
-    // const [ animated, setAnimated ] = useState(false) 
+  const callback = (entries) => {
+    const [entry] = entries;
+    setIsVisable(entry.isIntersecting);
+  };
 
-    const callback = entries => {
-        const [ entry ] = entries;
-        setIsVisable(entry.isIntersecting);
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
     };
 
-    useEffect(() => {
-        const options = {
-            root : null,
-            rootMargin : "0px",
-            threshold : 1.0
-        };
+    const observer = new IntersectionObserver(callback, options);
+    if (containerRef.current) observer.observe(containerRef.current);
 
-        const observer = new IntersectionObserver(callback, options);
-        if (containerRef.current) observer.observe(containerRef.current);
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current);
+    };
+  }, []);
 
-        return () => {
-            if (containerRef.current) observer.unobserve(containerRef.current);
-        };
-    }, [containerRef])
-
-    return (
-        <div className="graphics">
-            <div className={`graphics__line ${isVisible ? "graphics__line--animate" : ""}`} ref={containerRef}></div>
-            <div className="graphics__hexagons">
-                <div className="graphics__hexagon graphics__hexagon--outer"></div>
-                <div className="graphics__hexagon graphics__hexagon--inner"></div>
-            </div>
-        </div>
-    );
-    
+  return (
+    <div className="graphics">
+      <div
+        className={`graphics__line ${
+          isVisible ? "graphics__line--animate" : ""
+        }`}
+        ref={containerRef}
+      ></div>
+      <div className="graphics__hexagons">
+        <div className="graphics__hexagon graphics__hexagon--outer"></div>
+        <div className="graphics__hexagon graphics__hexagon--inner"></div>
+      </div>
+    </div>
+  );
 }
 
 export default HexagonGraphic;
