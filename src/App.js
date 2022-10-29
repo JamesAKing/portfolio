0d456b7d4c32c95b88d9c9163524e5a31924447a
James King <jamesking@catalystvr.com.au>
2022-10-29 13:29:50 +0100

    removed debug code"

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import './App.scss';
-import { useEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import ScrollToTop from './components/scroll-to-top/ScrollToTop';
-import Header from './components/header/Header';
-import AboutPage from './pages/about';
-import ContactMePage from './pages/contact-me';
-import HomePage from './pages/home';
+import "./App.scss";
+import { useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
+import Header from "./components/header/Header";
+import AboutPage from "./pages/about";
+import ContactMePage from "./pages/contact-me";
+import HomePage from "./pages/home";
 // import ProjectsPage from './pages/projects';
-import ProjectPage from './pages/project';
-import ResumePage from './pages/resume';
-import ContactBubble from './components/cta-contact-bubble/ContactBubble';
-import Footer from './components/footer/Footer';
-import { homeURL, aboutURL, resumeURL, contactMeURL, projectsURL } from './utilities/URLS';
+import ProjectPage from "./pages/project";
+import ResumePage from "./pages/resume";
+import ContactBubble from "./components/cta-contact-bubble/ContactBubble";
+import Footer from "./components/footer/Footer";
+import {
+  homeURL,
+  aboutURL,
+  resumeURL,
+  contactMeURL,
+  projectsURL,
+} from "./utilities/URLS";
 
 function App() {
+  // useEffect(() => {
+  //   // Broswer height fix not curently working.
+  //   if (typeof window === undefined) return console.error('Window is not defined');
 
-  useEffect(() => {
-    // Broswer height fix not curently working. 
-    if (typeof window === undefined) return console.error('Window is not defined');
+  //   const pixelHeight = window.innerHeight / 100;
+  //   const root = document.querySelector(':root');
 
-    const pixelHeight = window.innerHeight / 100;
-    const root = document.querySelector(':root');
-
-    if (root) root.style.setProperty('--vh', `${pixelHeight}px`);
-  }, []);
+  //   if (root) root.style.setProperty('--vh', `${pixelHeight}px`);
+  // }, []);
 
   return (
     <div className="App">
@@ -36,7 +41,7 @@ function App() {
           <Route path={aboutURL} component={AboutPage} />
           <Route path={resumeURL} component={ResumePage} />
           {/* <Route exact path={projectsURL} component={ProjectsPage} /> */}
-          <Route path={`${projectsURL}/:projectID`} component={ProjectPage} /> 
+          <Route path={`${projectsURL}/:projectID`} component={ProjectPage} />
           <Route path={contactMeURL} component={ContactMePage} />
         </Switch>
 
