1baec0565c34b425743e30ff83f2a6c2eb41f310
James King <jamesking@catalystvr.com.au>
2023-01-22 14:32:05 +0000

    fixed typo

--- src/components/projects-home/ProjectsHome.js
+++ src/components/projects-home/ProjectsHome.js
@@ -1,18 +1,28 @@
-import './ProjectsHome.scss';
-import { Link } from 'react-router-dom';
-import SectionHeader from '../section-header/SectionHeader';
-import ProjectCards from '../project-cards/ProjectCards';
-import { contactMeURL } from '../../utilities/URLS';
+import "./ProjectsHome.scss";
+import { Link } from "react-router-dom";
+import SectionHeader from "../section-header/SectionHeader";
+import ProjectCards from "../project-cards/ProjectCards";
+import { contactMeURL } from "../../utilities/URLS";
 
 function ProjectsHome() {
-    return (
-        <section className="projects-home">
-            <SectionHeader heading="PROJECTS" alt={true}/>
-            <p className="projects-home__text">WONDERING WHAT I DO WITH MY TIME?</p>
-            <ProjectCards />
-            <p className="projects-home__text">Due to the nature of the work, I am not able to show the code for all projects. If you have any questions regarding the above projects, please feel free to <Link to={contactMeURL} className="thank-you__text thank-you__text--highlight">react out!</Link></p>
-        </section>
-    );
+  return (
+    <section className="projects-home">
+      <SectionHeader heading="PROJECTS" alt={true} />
+      <p className="projects-home__text">WONDERING WHAT I DO WITH MY TIME?</p>
+      <ProjectCards />
+      <p className="projects-home__text">
+        Due to the nature of the work, I am unable to share code for all
+        projects. If you have any questions regarding the above projects, please
+        feel free to{" "}
+        <Link
+          to={contactMeURL}
+          className="thank-you__text thank-you__text--highlight"
+        >
+          reach out!
+        </Link>
+      </p>
+    </section>
+  );
 }
 
 export default ProjectsHome;
