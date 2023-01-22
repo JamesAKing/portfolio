7e60a254050a62df2bfbc8550089d103f878b0c1
James King <jamesking@catalystvr.com.au>
2023-01-22 10:48:37 +0000

    removed stale pages and urls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import "./App.scss";
+
 import { useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 import Header from "./components/header/Header";
 import AboutPage from "./pages/about";
 import ContactMePage from "./pages/contact-me";
 import HomePage from "./pages/home";
-// import ProjectsPage from './pages/projects';
-import ProjectPage from "./pages/project";
-import ResumePage from "./pages/resume";
 import ContactBubble from "./components/cta-contact-bubble/ContactBubble";
 import Footer from "./components/footer/Footer";
-import {
-  homeURL,
-  aboutURL,
-  resumeURL,
-  contactMeURL,
-  projectsURL,
-} from "./utilities/URLS";
+
+import { homeURL, aboutURL, contactMeURL } from "./utilities/URLS";
 
 function App() {
   useEffect(() => {
@@ -49,9 +43,6 @@ function App() {
         <Switch>
           <Route exact path={homeURL} component={HomePage} />
           <Route path={aboutURL} component={AboutPage} />
-          <Route path={resumeURL} component={ResumePage} />
-          {/* <Route exact path={projectsURL} component={ProjectsPage} /> */}
-          <Route path={`${projectsURL}/:projectID`} component={ProjectPage} />
           <Route path={contactMeURL} component={ContactMePage} />
         </Switch>
 
--- src/utilities/URLS.js
+++ src/utilities/URLS.js
@@ -2,8 +2,6 @@ const defaultURL = "";
 
 export const homeURL = `${defaultURL}/`;
 export const aboutURL = `${defaultURL}/about-me`;
-export const resumeURL = `${defaultURL}/resume`;
-export const projectsURL = `${defaultURL}/projects`;
 export const contactMeURL = `${defaultURL}/contact-me`;
 
 export const emailAddress = "james_andrew_king@hotmail.co.uk";
